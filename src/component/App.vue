7af178d97e5d8fb3283d6a4d23bc38199a73492f
gdmec07150814 <919316199@qq.com>
2017-12-20 11:17:28 +0800

    环境配置

---
+++ src/component/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        呵呵哒
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
+
+
---
+++ src/main.js
@@ -0,0 +1,10 @@
+// 1.1 导入第三方包
+import Vue from 'vue';
+
+// 导入根组件
+import AppComponent from './component/App.vue';
+
+new Vue({
+    el: '#app',
+    render: c => c(AppComponent)
+});
